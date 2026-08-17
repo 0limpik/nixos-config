1e31f416cacecbec21ec723f51413729c094d4ba
Evgeniy Galyuta <olimpik.net@gmail.com>
2026-08-17 11:27:12 +0300

    add flake

---
+++ flake.nix
@@ -0,0 +1,181 @@
+{
+  description = "Olimpik NixOS configuration";
+
+  inputs = {
+    nixpkgs-s.url = "github:nixos/nixpkgs/nixos-26.05";
+    nixpkgs-u.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-m.url = "github:nixos/nixpkgs/master";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-26.05";
+      inputs.nixpkgs.follows = "nixpkgs-s";
+    };
+
+    flake-compat = {
+      url = "github:NixOS/flake-compat";
+      flake = false;
+    };
+
+    olimpikpkgs = {
+      url = "path:./olimpikpkgs";
+      inputs.nixpkgs-s.follows = "nixpkgs-s";
+      inputs.nixpkgs-u.follows = "nixpkgs-u";
+      inputs.nixpkgs-m.follows = "nixpkgs-m";
+      inputs.flake-compat.follows = "flake-compat";
+    };
+
+    disko = {
+      url = "github:nix-community/disko/latest";
+      inputs.nixpkgs.follows = "nixpkgs-s";
+    };
+
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs-s";
+    };
+
+    catppuccin = {
+      url = "github:catppuccin/nix/release-26.05";
+      inputs.nixpkgs.follows = "nixpkgs-s";
+    };
+
+    vicinae = {
+      url = "github:vicinaehq/vicinae/v0.25.0"; # auto_update_tag
+      inputs.nixpkgs.follows = "nixpkgs-s";
+    };
+
+    vicinae-extensions = {
+      url = "github:vicinaehq/extensions";
+      inputs.nixpkgs.follows = "nixpkgs-s";
+      inputs.flake-compat.follows = "flake-compat";
+      inputs.vicinae.follows = "vicinae";
+    };
+
+    ayugram-desktop = {
+      url = "https://github.com/ndfined-crp/ayugram-desktop/";
+      type = "git";
+      submodules = true;
+      inputs.nixpkgs.follows = "nixpkgs-u";
+      inputs.tg_owt.inputs.nixpkgs.follows = "nixpkgs-u";
+    };
+
+    nix-jetbrains-plugins = {
+      url = "github:nix-community/nix-jetbrains-plugins";
+      inputs.nixpkgs.follows = "nixpkgs-u";
+      inputs.flake-compat.follows = "flake-compat";
+    };
+  };
+
+  outputs =
+    inputs@{
+      self,
+      nixpkgs-s,
+      nixpkgs-u,
+      nixpkgs-m,
+      home-manager,
+      olimpikpkgs,
+      ...
+    }:
+    let
+      system = "x86_64-linux";
+
+      pkgs-s = import ./olimpikpkgs/config/nixpkgs-s.nix system nixpkgs-s;
+      pkgs-u = import ./olimpikpkgs/config/nixpkgs-u.nix system nixpkgs-u;
+      pkgs-m = import ./olimpikpkgs/config/nixpkgs-m.nix system nixpkgs-m;
+      pkgs-o = olimpikpkgs.packages."${system}";
+      lib-o =
+        source:
+        olimpikpkgs.lib."${system}" {
+          inherit source;
+          path = ./.;
+          inherit (pkgs-s) lib;
+        };
+
+      args = {
+        inherit
+          system
+          inputs
+          pkgs-s
+          pkgs-u
+          pkgs-m
+          pkgs-o
+          ;
+      };
+      systemArgs = {
+        inherit system;
+        pkgs = pkgs-s;
+        specialArgs = args // {
+          lib-o = lib-o "system";
+          osConfig = null;
+          isSystem = true;
+          packages = pkgs: {
+            environment.systemPackages = pkgs;
+          };
+        };
+      };
+      homeArgs = args // {
+        lib-o = lib-o "home";
+        isSystem = false;
+        packages = pkgs: {
+          home.packages = pkgs;
+        };
+      };
+      modules = [
+        ./config/.
+        ./config/olimpik/.
+        olimpikpkgs.homeManagerModules
+        ./modules/module-list.nix
+        inputs.sops-nix.nixosModules.sops
+        ./config/sops.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager = {
+            extraSpecialArgs = homeArgs;
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            backupFileExtension = "backup";
+            sharedModules = [
+              inputs.sops-nix.homeManagerModules.sops
+              olimpikpkgs.homeManagerModules
+              ./modules/module-list.nix
+            ];
+            users.olimpik = ./config/olimpik/home.nix;
+          };
+        }
+        inputs.catppuccin.nixosModules.catppuccin
+      ];
+    in
+    {
+      nixosConfigurations = {
+        pc-main = nixpkgs-s.lib.nixosSystem (
+          systemArgs
+          // {
+            modules = modules ++ [
+              ./config/MS-7B85
+            ];
+          }
+        );
+        pc-mini = nixpkgs-s.lib.nixosSystem (
+          systemArgs
+          // {
+            modules = modules ++ [
+              ./config/G1619-04
+            ];
+          }
+        );
+      };
+      homeConfigurations = {
+        olimpik = home-manager.lib.homeManagerConfiguration {
+          extraSpecialArgs = homeArgs;
+          pkgs = pkgs-s;
+          modules = [
+            inputs.sops-nix.homeManagerModules.sops
+            olimpikpkgs.homeManagerModules
+            ./config/sops.nix
+            ./config/olimpik/home.nix
+            ./modules/module-list.nix
+          ];
+        };
+      };
+    };
+}
