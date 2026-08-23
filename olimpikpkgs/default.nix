25da112621ece1de27040c7030411374b2db1d99
Evgeniy Galyuta <olimpik.net@gmail.com>
2026-08-23 03:15:16 +0300

    add glitch update scripts

--- olimpikpkgs/default.nix
+++ olimpikpkgs/default.nix
@@ -1,14 +1,18 @@
 let
-  flake =
-    (import (
-      let
-        lock = builtins.fromJSON (builtins.readFile ./flake.lock);
-      in
-      fetchTarball {
-        url = "https://github.com{lock.nodes.flake-compat.locked.rev}.tar.gz";
-        sha256 = lock.nodes.flake-compat.locked.narHash;
-      }
-    ) { src = ./.; }).defaultNix;
   system = "x86_64-linux";
+  lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+  nodeName = lock.nodes.root.inputs.flake-compat;
+  flakeCompatSrc = builtins.fetchTarball {
+    url =
+      lock.nodes.${nodeName}.locked.url
+        or "https://github.com/edolstra/flake-compat/archive/${lock.nodes.${nodeName}.locked.rev}.tar.gz";
+    sha256 = lock.nodes.${nodeName}.locked.narHash;
+  };
+  flakeCompat = import flakeCompatSrc;
+  flake =
+    (flakeCompat {
+      src = ./.;
+      inherit system;
+    }).defaultNix;
 in
 flake.packages.${system}
--- olimpikpkgs/glitch/update-pkgs.sh
+++ olimpikpkgs/glitch/update-pkgs.sh
@@ -1,3 +1,41 @@
+nix_update() {
+  local package="${1:?package at first argument is required}"
+
+  # fix
+  # 293|   defaultNix =
+  #    |   ^
+  # 294|     builtins.removeAttrs result [ "__functor" ]
+  # and
+  # 222|       flake = import (outPath + "/flake.nix");
+  local flake_path=""
+  flake_path="$(nix store add-path --name olimpikpkgs ./)" || return
+  nix build \
+    --extra-experimental-features 'flakes nix-command' \
+    --print-out-paths --impure --expr "
+      with import <nixpkgs> { };
+      let
+        pkg = (
+          let
+            flake = builtins.getFlake \"${flake_path}\";
+          in
+          flake.packages.\${builtins.currentSystem}.\"davinci-resolve-studio\" or flake.\"davinci-resolve-studio\"
+        );
+      in
+      (pkgs.writeScript \"updateScript\" (
+        lib.escapeShellArgs (pkgs.lib.toList (pkg.updateScript.command or pkg.updateScript))
+      ))
+    " >/dev/null 2>&1 || return
+  nix store add-path --name olimpikpkgs ./ >/dev/null 2>&1 || return
+
+  nix-update \
+    --quiet \
+    --flake \
+    --use-update-script \
+    "$package" || return
+
+  rm --force "result"
+}
+
 update_scope() {
   local scope="${1:?scope at first argument is required}"
 
@@ -13,14 +51,69 @@ update_scope() {
   echo "count: ${#packages[@]}" >&2
   printf '=%.0s' {1..16} >&2
   echo >&2
-  xargs \
-    --max-args 1 \
-    --max-procs "${user_key#*:}" \
-    nix-update \
-    --quiet \
-    --flake \
-    --use-update-script \
-    <<<"${packages[@]}" || return
-  rm "result"
+
+  local processes="${user_key#*:}"
+  (
+
+    if ((processes == 1)); then
+      for package in "${packages[@]}"; do
+        echo "update: $package"
+        nix_update "$package"
+      done
+    else
+      export -f nix_update
+      printf '%s\n' "${packages[@]}" |
+        xargs \
+          --max-args 1 \
+          --max-procs "${user_key#*:}" \
+          bash -c 'nix_update "$1"' _ ||
+        return
+    fi
+  )
 }
 
+update() {
+  declare -A update
+
+  for key in "packages:4" "vscode_extensions:8"; do
+    update[$key]=0
+  done
+
+  while [[ $# -gt 0 ]]; do
+    local known=0
+    for key in "${!update[@]}"; do
+      user_key="${1//-/_}"
+      if [[ $user_key == "${key%:*}" ]]; then
+        update[$key]=1
+        shift 1
+        known=1
+      fi
+    done
+    if [[ $known == 0 ]]; then
+      echo "unknown key! $1" >&2
+      exit 1
+    fi
+  done
+
+  local all=1
+  for value in "${update[@]}"; do
+    if ((value == 1)); then
+      all=0
+      break
+    fi
+  done
+  if ((all == 1)); then
+    for key in "${!update[@]}"; do
+      update[$key]=1
+    done
+  fi
+
+  for key in "${!update[@]}"; do
+    value="${update[$key]}"
+    if [[ $value == 0 ]]; then
+      continue
+    fi
+
+    update_scope "$key" || return
+  done
+}
